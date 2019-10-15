0b6b2894c3b8b4943472531994c76c5444bff525
George Calin <george.calin@lacon.de>
2019-10-15 11:02:20 +0300

    Completed the code samples for explicit implementation of Interfaces in C#.

--- OddNumbers/Program.cs
+++ OddNumbers/Program.cs
@@ -7,6 +7,15 @@ namespace OddNumbers
         static void Main(string[] args)
         {
             Console.WriteLine ("Odd Numbers:");
+            var myObject = new OddGenerator();
+            foreach (var item in myObject)
+            {
+                if (item>50)
+                {
+                    break;          
+                }
+                Console.WriteLine(item);
+            }
 
             Console.Read();
         }
